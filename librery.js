dd3497b686761b4bfba8f57a2e3c6345cba4ff34
shubham-pawar01 <shubhamdev@mrchms.com>
2025-10-10 23:02:28 +0530

    fitst Commit

---
+++ librery.js
@@ -0,0 +1,15 @@
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function Substract(a, b) {
+//     return a - b;
+// }
+
+// // Export only the 'add' function
+// module.exports = { add };
+
+function add(a, b){
+    return a + b ;
+}
+module.exports = { add };
---
+++ main.js
@@ -0,0 +1,10 @@
+// const lib = require('./librery.js'); // Ensure the file name matches exactly
+
+// const res = lib.add(10, 2);
+// console.log(res); // This should print 3
+ 
+
+    const hello = require('./   librery.js');
+
+    const res = hello.add(10,2);
+    console.log(res);
